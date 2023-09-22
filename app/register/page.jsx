ef8c7ac5b663d2cad2ffe3a7f7fc19ad1475deae
Ninym2545 <Ninym250245@gmail.com>
2023-09-22 23:39:50 +0700

    edit register

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import dynamic from 'next/dynamic';
 
 const MyClientComponent = dynamic(() => import('../../components/Pages/register/MyComponents'), {
-  ssr: false, // Treat this component as a "Client Component"
+  ssr: false, // Treat this component as a "Client Component" ss
 });
 export const metadata = {
   title: 'Register',
 }
 const page = () => {
   return (
+
     <MyClientComponent />
   )
 }
